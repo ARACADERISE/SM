4cd8c1573ad6a6780f8e5d90137b61e1001ea9ed
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2021-02-25 14:54:54 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-chmod +x run
+if [ ! -d /bin/run ]; then
+        sudo cp /home/c_master/sm/run /usr/local/bin
+        chmod +x /usr/local/bin/run
+        echo "Type 'run' to run the program!"
+fi

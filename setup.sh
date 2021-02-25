90627204920e446e0444d384c1fca993434c7ec8
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2021-02-25 15:00:36 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 if [ ! -d /bin/run ]; then
-        sudo cp /home/c_master/sm/run /usr/local/bin
+        HOME_=$(readlink -f "$0")
+        HOME_PATH=$(dirname "$HOME_")
+        sudo cp $HOME_PATH/run /usr/local/bin
         chmod +x /usr/local/bin/run
         echo "Type 'run' to run the program!"
 fi

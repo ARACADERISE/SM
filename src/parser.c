80a2cce7651a7152cbc0e6f5dd3c9afcb534586b
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2021-03-20 02:35:43 -0500

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,7 @@
 #include "parser.h"
 
+char* flags;
+
 Parser_* init_parser(Lexer_* lexer)
 {
 	Parser_* parser = calloc(1,sizeof(*parser));
@@ -27,6 +29,7 @@ void print_err(Parser_* parser)
 Parser_* parse_user_define(Parser_* parser)
 {
 	// save variable name to syntax tree here
+redo:
 	get_next_token(parser);
 	if(!(parser->tokens->TokenId == Colon))
 	{
@@ -38,6 +41,9 @@ Parser_* parse_user_define(Parser_* parser)
 	{
 		case String:
 		{
+			// do something to store the value
+			flags = calloc(strlen(parser->tokens->token_value) + 2, sizeof(*flags));
+			flags = parser->tokens->token_value;
 			get_next_token(parser);
 			break;
 		}
@@ -46,12 +52,41 @@ Parser_* parse_user_define(Parser_* parser)
 			print_err(parser);
 		}
 	}
+	if(parser->tokens->TokenId == Id) goto redo;
+	return parser;
+}
+
+Parser_* parse_echo(Parser_* parser)
+{
+	get_next_token(parser);
+	if(parser->tokens->TokenId == String)
+	{
+		if(strcmp(parser->tokens->token_value, "$flags")==0)
+		{
+			printf("%s",flags);
+		} else
+		{
+			printf("%s",parser->tokens->token_value);
+		}
+		get_next_token(parser);
+	} else
+	{
+		fprintf(stderr,"Error, line %d:\n\techo expects two double quotes.\n\tSyntax:\n\t\techo \"Something To Print Here\"\n", parser->lexer->line-1);
+		exit(EXIT_FAILURE);
+	}
 	return parser;
 }
 
 Parser_* check_value(Parser_* parser)
 {
-	if(parser->tokens->TokenId == 0) return parse_user_define(parser);
+redo_:
+	switch(parser->tokens->TokenId)
+	{
+		case Id: parse_user_define(parser);break;
+		case Echo: parse_echo(parser);break;
+		default: break;
+	}
+	if(!(parser->tokens->TokenId == Eof)) goto redo_;
 	return parser;
 }
 
